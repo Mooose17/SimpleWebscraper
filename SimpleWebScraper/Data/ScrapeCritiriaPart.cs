6e400efdea385268f2c62ce65961d98be087a4a3
Mooose17 <tom@moose17.dev>
2023-07-20 12:12:16 +0100

    create class for criteria parts that has getters and setters

---
+++ SimpleWebScraper/Data/ScrapeCritiriaPart.cs
@@ -0,0 +1,9 @@
+using System.Text.RegularExpressions;
+
+namespace SimpleWebScraper.Data;
+
+public class ScrapeCritiriaPart
+{
+    public string Regex { get; set; }
+    public RegexOptions RegexOptions { get; set; }
+}
