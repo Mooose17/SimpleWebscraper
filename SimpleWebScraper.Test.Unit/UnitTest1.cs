6537fd6684c5045549843904dc0308fdf744bf9e
Mooose17 <tom@moose17.dev>
2023-07-20 12:02:41 +0100

    initialise repo with IDE files

---
+++ SimpleWebScraper.Test.Unit/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace SimpleWebScraper.Test.Unit;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
